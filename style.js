3ab4ba8f48fb7121547cf56b9c5f6e97e59b8346
ASMaa-AHMED146 <158354808+ASMaa-AHMED146@users.noreply.github.com>
2025-01-26 14:38:29 +0200

    transtaion

---
+++ style.js
@@ -0,0 +1,9 @@
+let submit=document.querySelector("form button");
+let form=document.querySelector("form ");
+let age=form.querySelector("input[name='myage']");
+console.log(age);
+let name=form.querySelector("input[name='myname']")
+submit.onclick=function(e)
+{
+    // e.preventDefault();
+}
